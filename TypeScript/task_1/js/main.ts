95925e7e4fadd874c8dcc6faa0dc738ec411cd1c
f-qrm <qerimifjolla7@gmail.com>
2025-09-09 15:09:51 +0200

    feat(task_1): implement Teacher and Directors interfaces, printTeacher function, and StudentClass
    - Create Teacher interface with readonly and optional properties
    - Create Directors interface that extends Teacher
    - Implement printTeacher function and its interface
    - Implement StudentInterface and StudentClass with constructor and methods workOnHomework & displayName
    - Add example objects for testing

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,10 +1,10 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
 const teacher3: Teacher = {
@@ -41,3 +41,26 @@ interface printTeacherFunction {
 }
 
 console.log(printTeacher("John", "Doe"));
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName
+  }
+}
